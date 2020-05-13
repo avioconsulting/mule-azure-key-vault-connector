53e31d29432c0d663417639ca3bde9f07fc32dc8
Kevin King <kking@avioconsulting.com>
2020-05-13 12:22:26 -0400

    Missing Exception

---
+++ src/main/java/com/avioconsulting/mule/connector/akv/provider/api/error/CertificateNotFoundException.java
@@ -0,0 +1,9 @@
+package com.avioconsulting.mule.connector.akv.provider.api.error;
+
+import org.mule.runtime.extension.api.exception.ModuleException;
+
+public class CertificateNotFoundException extends ModuleException {
+    public CertificateNotFoundException(String message) {
+        super(message, AzureKeyVaultErrorType.CERTIFICATE_NOT_FOUND);
+    }
+}
